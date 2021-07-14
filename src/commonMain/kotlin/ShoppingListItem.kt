7c587a7b07412cf64f9e8369fc6f66252437a57d
Toumani <toumani49@gmail.com>
2021-07-14 10:46:04 +0000

    Initial commit

---
+++ src/commonMain/kotlin/ShoppingListItem.kt
@@ -0,0 +1,13 @@
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ShoppingListItem(
+    val description: String,
+    val priority: Int
+) {
+    val id: Int = description.hashCode()
+
+    companion object {
+        const val path = "/shoppingList"
+    }
+}
---
+++ src/jvmMain/kotlin/Server.kt
@@ -0,0 +1,67 @@
+import com.mongodb.ConnectionString
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.eq
+import org.litote.kmongo.reactivestreams.KMongo
+
+fun main() {
+	val connectionString: ConnectionString? = System.getenv("MONGODB_URI")?.let {
+		ConnectionString("$it?retryWrites=false")
+	}
+
+	val client = if (connectionString != null) KMongo.createClient(connectionString).coroutine else KMongo.createClient().coroutine
+	val database = client.getDatabase(connectionString?.database ?: "shoppingList")
+	val collection = database.getCollection<ShoppingListItem>()
+
+	val port = System.getenv("PORT")?.toInt() ?: 9090
+
+	embeddedServer(Netty, port) {
+		install(ContentNegotiation) {
+			json()
+		}
+		install(CORS) {
+			method(HttpMethod.Get)
+			method(HttpMethod.Post)
+			method(HttpMethod.Delete)
+			anyHost()
+		}
+		install(Compression) {
+			gzip()
+		}
+
+		routing {
+			get("/") {
+				call.respondText (
+					this::class.java.classLoader.getResource("index.html")!!.readText(),
+					ContentType.Text.Html
+				)
+			}
+			static("/") {
+				resources("")
+			}
+			route(ShoppingListItem.path) {
+				get {
+					call.respond(collection.find().toList())
+				}
+				post {
+					collection.insertOne(call.receive<ShoppingListItem>())
+					call.respond(HttpStatusCode.OK)
+				}
+				delete("/{id}") {
+					val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+					collection.deleteOne(ShoppingListItem::id eq id)
+					call.respond(HttpStatusCode.OK)
+				}
+			}
+		}
+	}.start(true)
+}
